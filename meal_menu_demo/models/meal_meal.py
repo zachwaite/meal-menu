6fe7e380f598fcd0214e089432e0df44f633e047
Zach Waite <zach@waiteperspectives.com>
2019-09-03 22:26:43 +0000

    [IMP] meal_menu_demo: Add domain filters on meal items

--- meal_menu_demo/models/meal_meal.py
+++ meal_menu_demo/models/meal_meal.py
@@ -10,3 +10,18 @@ class Meal(models.Model):
     appetizer_2 = fields.Many2one(comodel_name='meal.item')
     dessert_1 = fields.Many2one(comodel_name='meal.item')
 
+    entree_filter = fields.Many2one(
+        comodel_name='meal.item.category',
+        default=lambda self: self.env.ref('meal_menu_demo.category_entree').id,
+    )
+
+    appetizer_filter = fields.Many2one(
+        comodel_name='meal.item.category',
+        default=lambda self: self.env.ref('meal_menu_demo.category_appetizer').id,
+    )
+
+    dessert_filter = fields.Many2one(
+        comodel_name='meal.item.category',
+        default=lambda self: self.env.ref('meal_menu_demo.category_dessert').id,
+    )
+
