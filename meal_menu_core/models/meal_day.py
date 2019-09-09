3a95fb14c0b6b15db6ff8350e60ecab9d9822382
Zach Waite <zach@waiteperspectives.com>
2019-09-09 15:07:18 +0000

    [IMP] meal_menu_core: Add meal label

--- meal_menu_core/models/meal_day.py
+++ meal_menu_core/models/meal_day.py
@@ -15,6 +15,12 @@ class MealDay(models.Model):
         readonly=True,
     )
 
+    meal_label = fields.Char(
+        string='Meal Day',
+        compute='_compute_meal_label',
+        store=True,
+    )
+
     meal_ids = fields.One2many(
         comodel_name='meal.meal',
         inverse_name='meal_day_id',
@@ -31,6 +37,14 @@ class MealDay(models.Model):
         comodel_name='meal.item',
         compute='_compute_meal_item_ids',
     )
+
+    @api.multi
+    @api.depends('meal_date')
+    def _compute_meal_label(self):
+        # TODO: use special labels
+        for record in self:
+            record.meal_label = record.get_meal_label(record.meal_date, False)
+
 
     @api.multi
     def _compute_meal_item_ids(self):
@@ -58,4 +72,15 @@ class MealDay(models.Model):
         meal = self.meal_ids.filtered(lambda m: m.meal_time_id.key == key)
         meal[slot] = val
 
+
+    def get_meal_label(self, meal_date, special_label):
+        """Designed to allow injecting a special label instead of normal weekday
+        """
+        if special_label:
+            return special_label
+        else:
+            return self.get_meal_weekday(meal_date)
+
+    def get_meal_weekday(self, meal_date):
+        return meal_date.strftime('%a')
 
